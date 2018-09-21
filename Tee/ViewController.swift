b789fdc7a6dcafacef6920d0617544084da96dca
Prakhar <iprakhar@ymail.com>
2018-09-20 21:51:21 -0700

    Initial Commit

---
+++ Tee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tee
+//
+//  Created by Prakhar Tripathi on 2018-09-20.
+//  Copyright © 2018 ThirdEyeOrganization. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
