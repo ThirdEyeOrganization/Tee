d5beebb31d94eb62edf8bc620f89845bc7f21851
Adi <aditya.chinchure@gmail.com>
2018-10-23 20:36:54 -0700

    initial commit

---
+++ Tee/LocationManager.swift
@@ -0,0 +1,36 @@
+//
+//  LocationManager.swift
+//  Tee
+//
+//  Created by Aditya Chinchure on 2018-10-23.
+//  Copyright © 2018 ThirdEyeOrganization. All rights reserved.
+//
+
+import AVFoundation
+import MapKit
+import CoreLocation
+
+
+class LocationManager: NSObject, CLLocationManagerDelegate{
+    
+    let locationManager = CLLocationManager()
+    
+    func returnLatLong() -> [String] {
+        locationManager.requestWhenInUseAuthorization()
+        var currentLocation: CLLocation!
+        var locArr:[String] = []
+        
+        if( CLLocationManager.authorizationStatus() == .authorizedWhenInUse ||
+            CLLocationManager.authorizationStatus() ==  .authorizedAlways){
+            
+            currentLocation = locationManager.location
+            
+            locArr.append("\(currentLocation.coordinate.latitude)")
+            locArr.append("\(currentLocation.coordinate.longitude)")
+            
+        }
+        
+        return locArr
+    }
+    
+}
